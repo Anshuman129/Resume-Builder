b38b075b9b2c46ff421e19192ec6e9317410f4da
Anshuman129 <168343354+Anshuman129@users.noreply.github.com>
2025-06-12 16:09:25 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import { React, useContext } from 'react'
 import Introduction from '../../Components/Intro/Introduction'
 import ResumeContext from '../../Context/ResumeContext';
 import BuilderArea from '../BuilderArea';
-import Theme1 from './../../Theme/Theme1/Theme1'
+import Theme1 from './../../Theme/Theme1/Theme1';
 import Theme2 from '../../Theme/Theme2/Theme2';
 import Theme3 from '../../Theme/Theme3/Theme3';
 import Theme5 from '../../Theme/Theme5/Theme5';
 import Theme6 from '../../Theme/Theme6/Theme6';
 import Theme7 from '../../Theme/Theme7/Theme7';
+import Theme8 from '../../Theme/Theme8/Theme8';
+import Theme9 from '../../Theme/Theme9/Theme9';
+import Theme10 from '../../Theme/Theme10/Theme10';
 import ErrorPage from '../Error/ErrorPage';
 
 const Home = () => {
@@ -35,6 +38,15 @@ const Home = () => {
             }
             {
                 (showComponent && currentTheme === 'Theme7') && <BuilderArea theme={<Theme7 componentRef={componentRef} themeData={themeData} />} />
+            }
+            {
+                (showComponent && currentTheme === 'Theme8') && <BuilderArea theme={<Theme8 componentRef={componentRef} themeData={themeData} />} />
+            }
+            {
+                (showComponent && currentTheme === 'Theme9') && <BuilderArea theme={<Theme9 componentRef={componentRef} themeData={themeData} />} />
+            }
+            {
+                (showComponent && currentTheme === 'Theme10') && <BuilderArea theme={<Theme10 componentRef={componentRef} themeData={themeData} />} />
             }
             {
                 (showComponent && currentTheme === 'Theme4') && <ErrorPage />
